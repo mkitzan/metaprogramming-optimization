fbaffa019cb5ec33e4a15396f26d64d2fcf2344a
Michael Kitzan <mkitzan45@gmail.com>
2020-01-17 11:54:02 -0800

    Fisher Price(c) Baby's First: Fold Expression

--- res/str_template.cpp
+++ res/str_template.cpp
@@ -1,33 +1,51 @@
 #include <iostream>
+#include <cstdarg>
+#include <initializer_list>
+#include <array>
 #include "cexpr_string.hpp"
 
 template<cexpr_string S>
 class Equation {
 public:
-	static constexpr int eval(int x, int y) 
+	template<typename ...T>
+	static constexpr int eval(T&&... args) 
 	{
-		int res{};
+		std::array<int, sizeof...(args)> values{ { args... } };
+		int acc{};
+		char op{};
+		bool start{ true };
 
-		switch(S[1]) 
+		for (auto c : S) 
 		{
-		case '+':
-			res = x + y; break;
-		case '-':
-			res = x - y; break;
-		case '*':
-			res = x * y; break;
-		case '/':
-			res = x / y; break;
+			if (op == 0 && !start)
+			{
+				op = c;
+			}
+			else
+			{
+				if (op == '*')
+				{
+					acc *= values[c - 'a'];
+				}
+				else
+				{
+					acc += values[c - 'a'];
+				}
+
+				op = 0;
+			}
+
+			start = false;
 		}
 
-		return res;
+		return acc;
 	}
 };
 
 // trust me it works with g++-9.2
 int main()
 {
-	constexpr auto result{ Equation<"x+y">::eval(2, 2) };
+	constexpr auto result{ Equation<"a+b+a">::eval(2, 3) };
 	std::cout << result << std::endl;
 	return 0;
 }
